8cf9a310caa787be2d6312a1c483b46c702e5c4a
dejiyama <deziyama0404@gmail.com>
2020-10-02 21:14:05 +0900

    create files of firebase authentication

---
+++ src/firebase/config.js
@@ -0,0 +1,11 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+export const firebaseConfig = {
+    apiKey: "AIzaSyDTer6AaBCZMMrG5-vOM20hmoioBxBsWsI",
+    authDomain: "ec-app-dev-b424f.firebaseapp.com",
+    databaseURL: "https://ec-app-dev-b424f.firebaseio.com",
+    projectId: "ec-app-dev-b424f",
+    storageBucket: "ec-app-dev-b424f.appspot.com",
+    messagingSenderId: "672976794305",
+    appId: "1:672976794305:web:f8ec99ecfca6fefd82da40",
+    measurementId: "G-559V7MB8BS"
+  };
