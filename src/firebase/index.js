b5a4a86609ba1837aa71f5ef5bae4f75cf0d3e4f
dejiyama <deziyama0404@gmail.com>
2020-10-05 14:15:06 +0900

    create authentication function with firebase

--- src/firebase/index.js
+++ src/firebase/index.js
@@ -1,5 +1,6 @@
 import firebase from 'firebase/app'
 import 'firebase/auth'
+import 'firebase/firestore'
 import 'firebase/storage'
 import 'firebase/functions'
 import { firebaseConfig } from "./config"
@@ -8,9 +9,9 @@ import { firebaseConfig } from "./config"
 firebase.initializeApp(firebaseConfig);
 
 //firebaseのメソッドをconstで宣言した変数に入れている。ちなみにメソッドとは、プロパティに値が関数のものをメソッドとよぶ。
-export const auth = firebase.auth()
-export const db = firebase.firestore()
-export const storage = firebase.storage()
-export const functions = firebase.functions()
+export const auth = firebase.auth();
+export const db = firebase.firestore();
+export const storage = firebase.storage();
+export const functions = firebase.functions();
 //下記のメソッドは、日時を取得できるので、作成した日時を取得したい時に使える。
-export const FirebaseTimestamp = firebase.firestore.Timestamp
+export const FirebaseTimestamp = firebase.firestore.Timestamp;
